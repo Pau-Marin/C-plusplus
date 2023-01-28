28a35000ea6a7aa67182a9d1ec3072e11235e61e
Pau-Marin <ritsu95@icloud.com>
2023-01-28 17:51:29 +0100

    Interfaces in C++

--- C-plusplus/src/Main.cpp
+++ C-plusplus/src/Main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
-class Entity
+class Printable
+{
+public:
+	virtual std::string GetClassName() = 0;
+};
+
+class Entity :public Printable
 {
 public:
 	virtual std::string GetName() { return "Entity"; }
+	std::string GetClassName() override { return "Entity"; }
 };
 
 class Player : public Entity
@@ -14,6 +21,7 @@ public:
 	Player(const std::string& name) :m_Name(name) {}
 
 	std::string GetName() override { return m_Name; }
+	std::string GetClassName() override { return "Player"; }
 };
 
 void PrintName(Entity* entity)
@@ -21,13 +29,28 @@ void PrintName(Entity* entity)
 	std::cout << entity->GetName() << std::endl;
 }
 
+void Print(Printable* obj)
+{
+	std::cout << obj->GetClassName() << std::endl;
+}
+
+class A : public Printable
+{
+public:
+	std::string GetClassName() override { return "A"; }
+};
+
 int main()
 {
 	Entity* e = new Entity();
-	PrintName(e);
+	// PrintName(e);
 
 	Player* p = new Player("Ritsu");
-	PrintName(p);
+	// PrintName(p);
+
+	Print(e);
+	Print(p);
+	Print(new A());
 
 	std::cin.get();
 }

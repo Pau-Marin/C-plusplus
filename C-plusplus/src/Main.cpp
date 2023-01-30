b94b4e055ac3486e9a4ecca4830efcb7533bd35a
Pau-Marin <ritsu95@icloud.com>
2023-01-30 11:47:23 +0100

    Member initializer lists in C++

--- C-plusplus/src/Main.cpp
+++ C-plusplus/src/Main.cpp
@@ -1,31 +1,45 @@
 #include <iostream>
 
+class Example
+{
+public:
+	Example()
+	{
+		std::cout << "Created Entity!" << std::endl;
+	}
+
+	Example(int x)
+	{
+		std::cout << "Created Entity with " << x << std::endl;
+	}
+};
+
 class Entity
 {
 private:
 	std::string m_Name;
-	mutable int m_DebugCount = 0;
+	Example m_Example;
 public:
+	Entity()
+		:m_Example(8)
+	{
+		m_Name = std::string("Unknown");
+	}
+
+	Entity(const std::string& name)
+		: m_Name(name)
+	{
+	}
+
 	const std::string& GetName()const
 	{
-		m_DebugCount++;
 		return m_Name;
 	}
 };
 
 int main()
 {
-	const Entity e;
-	e.GetName();
-
-	int x = 8;
-	auto f = [=]() mutable
-	{
-		x++;
-		std::cout << x << std::endl;
-	};
-
-	f();
+	Entity e0;
 
 	std::cin.get();
 }

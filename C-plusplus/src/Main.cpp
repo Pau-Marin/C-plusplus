293992ce03caf7f562a9557c0a2d87ceb6cf7f7e
Pau-Marin <ritsu95@icloud.com>
2023-01-30 13:58:50 +0100

    How to create/instantiate objects in C++

--- C-plusplus/src/Main.cpp
+++ C-plusplus/src/Main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 
-static int s_Level = 6;
-static int s_Speed = 2;
+using String = std::string;
+
+class Entity
+{
+private:
+	String m_Name;
+public:
+	Entity() :m_Name("Unknown") {}
+	Entity(const String& name) : m_Name(name) {}
+
+	const String& GetName() const { return m_Name; }
+};
 
 int main()
 {
-	if (s_Level > 5)
-		s_Speed = 10;
-	else
-		s_Speed = 5;
-	s_Speed = s_Level > 5 && s_Level < 100 ? s_Level > 10 ? 15 : 10 : 5;
-
-	std::cout << "Level: " << s_Level << ", Speed: " << s_Speed << std::endl;
-
-	std::string otherRank;
-	if (s_Level > 10)
-		otherRank = "Master";
-	else
-		otherRank = "Begginer";
-
-	std::string rank = s_Level > 10 ? "Master" : "Begginer";
+	Entity* e;
+	{
+		Entity* entity = new Entity("Ritsu");
+		e = entity;
+		std::cout << entity->GetName() << std::endl;
+	}
 
 	std::cin.get();
+	delete e;
 }

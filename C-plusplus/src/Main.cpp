2d50185d801236a419534ecaf92378b8fe711f17
Pau-Marin <ritsu95@icloud.com>
2023-02-02 15:25:36 +0100

    Using libraries in C++ (Static Linking)

--- C-plusplus/src/Main.cpp
+++ C-plusplus/src/Main.cpp
@@ -1,29 +1,11 @@
 #include <iostream>
-#include <vector>
-
-struct Vertex
-{
-	float x, y, z;
-
-	Vertex(float x, float y, float z)
-		:x(x), y(y), z(z)
-	{
-	}
-
-	Vertex(const Vertex& vertex)
-		: x(vertex.x), y(vertex.y), z(vertex.z)
-	{
-		std::cout << "Copied!" << std::endl;
-	}
-};
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	std::vector<Vertex> vertices;
-	vertices.reserve(3);
-	vertices.emplace_back(1, 2, 3);
-	vertices.push_back(Vertex(4, 5, 6));
-	vertices.push_back(Vertex(7, 8, 9));
+	int a = glfwInit();
+
+	std::cout << a << std::endl;
 
 	std::cin.get();
 }

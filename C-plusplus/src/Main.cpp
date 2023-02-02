15ebf8927f43ad073261317b08d5fdf28b8b4a9d
Pau-Marin <ritsu95@icloud.com>
2023-02-02 14:47:08 +0100

    Optimizing the usage of std::Vector in C++

--- C-plusplus/src/Main.cpp
+++ C-plusplus/src/Main.cpp
@@ -4,38 +4,26 @@
 struct Vertex
 {
 	float x, y, z;
+
+	Vertex(float x, float y, float z)
+		:x(x), y(y), z(z)
+	{
+	}
+
+	Vertex(const Vertex& vertex)
+		: x(vertex.x), y(vertex.y), z(vertex.z)
+	{
+		std::cout << "Copied!" << std::endl;
+	}
 };
-
-std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
-{
-	stream << vertex.x << ", " << vertex.y << ", " << vertex.z;
-	return stream;
-}
-
-void Function(const std::vector<Vertex>& vertices)
-{
-
-}
 
 int main()
 {
 	std::vector<Vertex> vertices;
-	vertices.push_back({ 1,2,3 });
-	vertices.push_back({ 4,5,6 });
-
-	Function(vertices);
-
-	for (int i = 0; i < vertices.size(); i++)
-	{
-		std::cout << vertices[i] << std::endl;
-	}
-
-	vertices.erase(vertices.begin() + 1);
-
-	for (Vertex& v : vertices)
-	{
-		std::cout << v << std::endl;
-	}
+	vertices.reserve(3);
+	vertices.emplace_back(1, 2, 3);
+	vertices.push_back(Vertex(4, 5, 6));
+	vertices.push_back(Vertex(7, 8, 9));
 
 	std::cin.get();
 }
